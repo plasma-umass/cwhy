c538e639a94c275389cddb9655ecfa823d94edc0
Emery Berger <emery.berger@gmail.com>
2023-04-04 12:28:48 -0400

    Initial.

---
+++ src/c-why/c-why.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python3
+import asyncio
+import io
+import openai
+import openai_async
+import sys
+import textwrap
+
+def word_wrap_except_code_blocks(text: str) -> str:
+    """Wraps text except for code blocks.
+
+    Splits the text into paragraphs and wraps each paragraph,
+    except for paragraphs that are inside of code blocks denoted
+    by ` ``` `. Returns the updated text.
+
+    Args:
+        text: The text to wrap.
+
+    Returns:
+        The wrapped text.
+    """
+    # Split text into paragraphs
+    paragraphs = text.split('\n\n')
+    wrapped_paragraphs = []
+    # Check if currently in a code block.
+    in_code_block = False
+    # Loop through each paragraph and apply appropriate wrapping.
+    for paragraph in paragraphs:
+        # If this paragraph starts and ends with a code block, add it as is.
+        if paragraph.startswith('```') and paragraph.endswith('```'):
+            wrapped_paragraphs.append(paragraph)
+            continue
+        # If this is the beginning of a code block add it as is.
+        if paragraph.startswith('```'):
+            in_code_block = True
+            wrapped_paragraphs.append(paragraph)
+            continue
+        # If this is the end of a code block stop skipping text.
+        if paragraph.endswith('```'):
+            in_code_block = False
+            wrapped_paragraphs.append(paragraph)
+            continue
+        # If we are currently in a code block add the paragraph as is.
+        if in_code_block:
+            wrapped_paragraphs.append(paragraph)
+        else:
+            # Otherwise, apply text wrapping to the paragraph.
+            wrapped_paragraph = textwrap.fill(paragraph)
+            wrapped_paragraphs.append(wrapped_paragraph)
+    # Join all paragraphs into a single string
+    wrapped_text = '\n\n'.join(wrapped_paragraphs)
+    return wrapped_text
+
+
+with io.open(sys.stdin.fileno(), "rb", closefd=False) as stdin:
+     the_error = ' '.join([line.decode() for line in stdin.readlines()])
+
+"""
+    This is my code:
+{the_code}
+"""
+
+user_prompt=f"""
+This is my error:
+{the_error}
+
+What's the problem?
+"""
+
+async def complete(user_prompt):
+    try:
+        completion = await openai_async.chat_complete(openai.api_key, timeout=30, payload={'model': 'gpt-3.5-turbo', 'messages': [{'role': 'user', 'content': user_prompt}]})
+        json_payload = completion.json()
+        text = json_payload['choices'][0]['message']['content']
+    except (openai.error.AuthenticationError, httpx.LocalProtocolError, KeyError):
+        # Something went wrong.                                                                                                                                     
+        print()
+        print('You need a valid OpenAI key to use ChatDBG. You can get a key here: https://openai.com/api/')
+        print('Set the environment variable OPENAI_API_KEY to your key value.')
+        import sys
+        sys.exit(1)
+    except Exception as e:
+        print(f'EXCEPTION {e}, {type(e)}')
+        pass
+    return text
+
+text = asyncio.run(complete(user_prompt))
+print('\n'.join(textwrap.wrap(text, width=70)))
+
+
+    
