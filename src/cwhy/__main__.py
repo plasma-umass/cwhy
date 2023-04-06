e2b7dee05bbb29d7b6b4713db6c055522ee9505d
Nicolas van Kempen <nvankemp@gmail.com>
2023-04-06 18:37:33 -0400

    Add option to show prompt before sending it to OpenAI

--- src/cwhy/__main__.py
+++ src/cwhy/__main__.py
@@ -6,11 +6,16 @@ from . import cwhy
 
 @click.command()
 @click.option('--fix', is_flag=True, help="Instead of explaining error messages, propose a fix.", required=False, default=False)
-def main(fix):
+@click.option('--show-prompt', is_flag=True, help="Print the prompt before sending it to OpenAI for debugging.", required=False, default=False)
+def main(fix, show_prompt):
     prompt = cwhy.cwhy_prompt(fix)
     if not prompt:
         # Do nothing if nothing was sent to stdin
         return
+    if show_prompt:
+        print("===================== Prompt =====================")
+        print(prompt)
+        print("==================================================")
     text = asyncio.run(cwhy.complete(prompt))
     print(cwhy.word_wrap_except_code_blocks(text))
 
