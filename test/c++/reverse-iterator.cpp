4741f898c1d186c078ab2d81dae10e2b5dfdb186
Nicolas van Kempen <nvankemp@gmail.com>
2023-04-06 16:08:17 -0400

    Add example I ran into writing a trim function

---
+++ test/c++/reverse-iterator.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <string>
+
+std::string trim(std::string s) {
+  static const auto isNotSpace = [](auto c) { return !std::isspace(c); };
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), isNotSpace));
+  s.erase(std::find_if(s.rbegin(), s.rend(), isNotSpace), s.end());
+  return s;
+}
