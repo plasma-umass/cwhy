11f2c4da7026d5dc5bfa19b0c9a703ad5f11b8bd
Emery Berger <emery.berger@gmail.com>
2023-04-12 14:32:02 -0400

    Added --version flag.

--- src/cwhy/__main__.py
+++ src/cwhy/__main__.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import asyncio
 import click
+import importlib.metadata
 
 from . import cwhy
 
@@ -20,8 +21,12 @@ def evaluate_prompt(ctx, prompt, wrap = True):
 @click.option('--llm', type = click.Choice(['gpt-4', 'gpt-3.5-turbo']), required = False, default = 'gpt-3.5-turbo')
 @click.option('--show-prompt', is_flag = True, help = "Print the prompt before sending it to OpenAI for debugging.", required = False, default = False)
 @click.option('--timeout', type = int, required = False, default = 60, help = 'Timeout for API calls in seconds.')
+@click.option('--version', is_flag=True, help = "Print the version of cwhy.", required = False, default = False)
 @click.pass_context
-def main(ctx, llm, timeout, show_prompt):
+def main(ctx, llm, timeout, version, show_prompt):
+    if version:
+        print(f"cwhy version {importlib.metadata.metadata('ChatDBG')['Version']}")
+        return
     ctx.ensure_object(dict)
     ctx.obj['llm'] = llm
     ctx.obj['timeout'] = timeout
