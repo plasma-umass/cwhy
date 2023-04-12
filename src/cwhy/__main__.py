89fca5c48ea884b4612841b558c0a5d4477c621d
Nicolas van Kempen <nvankemp@gmail.com>
2023-04-12 14:16:10 -0400

    Add timeout CLI option

--- src/cwhy/__main__.py
+++ src/cwhy/__main__.py
@@ -12,18 +12,20 @@ def evaluate_prompt(ctx, prompt, wrap = True):
         print("===================== Prompt =====================")
         print(prompt)
         print("==================================================")
-    text = asyncio.run(cwhy.complete(ctx.obj['llm'], prompt))
+    text = asyncio.run(cwhy.complete(ctx, prompt))
     if wrap: text = cwhy.word_wrap_except_code_blocks(text)
     print(text)
 
 @click.group(invoke_without_command = True)
-@click.option('--show-prompt', is_flag = True, help = "Print the prompt before sending it to OpenAI for debugging.", required = False, default = False)
 @click.option('--llm', type = click.Choice(['gpt-4', 'gpt-3.5-turbo']), required = False, default = 'gpt-3.5-turbo')
+@click.option('--show-prompt', is_flag = True, help = "Print the prompt before sending it to OpenAI for debugging.", required = False, default = False)
+@click.option('--timeout', type = int, required = False, default = 60, help = 'Timeout for API calls in seconds.')
 @click.pass_context
-def main(ctx, show_prompt, llm):
+def main(ctx, llm, timeout, show_prompt):
     ctx.ensure_object(dict)
-    ctx.obj['show-prompt'] = show_prompt
     ctx.obj['llm'] = llm
+    ctx.obj['timeout'] = timeout
+    ctx.obj['show-prompt'] = show_prompt
 
     if ctx.invoked_subcommand is None:
         ctx.invoke(explain)
--- src/cwhy/cwhy.py
+++ src/cwhy/cwhy.py
@@ -80,9 +80,9 @@ def read_lines(file_path: str, start_line: int, end_line: int) -> (str, int):
     # return the requested lines as a list
     return ('\n'.join(lines[start_line:end_line]) + '\n', start_line, end_line)
 
-async def complete(llm, user_prompt):
+async def complete(ctx, user_prompt):
     try:
-        completion = await openai_async.chat_complete(openai.api_key, timeout=60, payload={'model': llm, 'messages': [{'role': 'user', 'content': user_prompt}]})
+        completion = await openai_async.chat_complete(openai.api_key, timeout=ctx.obj['timeout'], payload={'model': ctx.obj['llm'], 'messages': [{'role': 'user', 'content': user_prompt}]})
         completion.raise_for_status()
         text = completion.json()['choices'][0]['message']['content']
     except (openai.error.AuthenticationError, httpx.LocalProtocolError, httpx.HTTPStatusError):
