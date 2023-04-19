8b8af5a93daae83c98de51c22c54838964c017d9
Nicolas van Kempen <nvankemp@gmail.com>
2023-04-19 13:46:20 -0400

    Format [black]

--- src/cwhy/__main__.py
+++ src/cwhy/__main__.py
@@ -1,51 +1,80 @@
 #! /usr/bin/env python3
-import click
 import importlib.metadata
+
+import click
 
 from . import cwhy
 
-def evaluate_prompt(args, prompt, wrap = True):
+
+def evaluate_prompt(args, prompt, wrap=True):
     if not prompt:
         # Do nothing if nothing was sent to stdin
         return
-    if args['show-prompt']:
+    if args["show-prompt"]:
         print("===================== Prompt =====================")
         print(prompt)
         print("==================================================")
     text = cwhy.complete(args, prompt)
-    if wrap: text = cwhy.word_wrap_except_code_blocks(text)
+    if wrap:
+        text = cwhy.word_wrap_except_code_blocks(text)
     print(text)
 
-@click.group(invoke_without_command = True)
-@click.option('--llm', type = click.Choice(['gpt-4', 'gpt-3.5-turbo']), required = False, default = 'gpt-3.5-turbo')
-@click.option('--show-prompt', is_flag = True, help = "Print the prompt before sending it to OpenAI for debugging.", required = False, default = False)
-@click.option('--timeout', type = int, required = False, default = 60, help = 'Timeout for API calls in seconds.')
-@click.option('--version', is_flag=True, help = "Print the version of cwhy.", required = False, default = False)
+
+@click.group(invoke_without_command=True)
+@click.option(
+    "--llm",
+    type=click.Choice(["gpt-4", "gpt-3.5-turbo"]),
+    required=False,
+    default="gpt-3.5-turbo",
+)
+@click.option(
+    "--show-prompt",
+    is_flag=True,
+    help="Print the prompt before sending it to OpenAI for debugging.",
+    required=False,
+    default=False,
+)
+@click.option(
+    "--timeout",
+    type=int,
+    required=False,
+    default=60,
+    help="Timeout for API calls in seconds.",
+)
+@click.option(
+    "--version",
+    is_flag=True,
+    help="Print the version of cwhy.",
+    required=False,
+    default=False,
+)
 @click.pass_context
 def main(ctx, llm, timeout, version, show_prompt):
     if version:
         print(f"cwhy version {importlib.metadata.metadata('cwhy')['Version']}")
         return
     ctx.ensure_object(dict)
-    ctx.obj['llm'] = llm
-    ctx.obj['timeout'] = timeout
-    ctx.obj['show-prompt'] = show_prompt
+    ctx.obj["llm"] = llm
+    ctx.obj["timeout"] = timeout
+    ctx.obj["show-prompt"] = show_prompt
 
     if ctx.invoked_subcommand is None:
         ctx.invoke(explain)
 
-@main.command(short_help = 'Explain the diagnostic.')
+
+@main.command(short_help="Explain the diagnostic.")
 @click.pass_context
 def explain(ctx):
     evaluate_prompt(ctx.obj, cwhy.explain_prompt())
 
-@main.command(short_help = 'Propose a fix for the diagnostic.')
+
+@main.command(short_help="Propose a fix for the diagnostic.")
 @click.pass_context
 def fix(ctx):
     evaluate_prompt(ctx.obj, cwhy.fix_prompt())
 
-@main.command(short_help = 'Extract the source locations from the diagnostic as CSV.')
+
+@main.command(short_help="Extract the source locations from the diagnostic as CSV.")
 @click.pass_context
 def extract_sources(ctx):
-    evaluate_prompt(ctx.obj, cwhy.extract_sources_prompt(), wrap = False)
-
+    evaluate_prompt(ctx.obj, cwhy.extract_sources_prompt(), wrap=False)
--- src/cwhy/cwhy.py
+++ src/cwhy/cwhy.py
@@ -1,10 +1,10 @@
-#! /usr/bin/env python3
 import io
 import re
 import sys
 import textwrap
 
 import openai
+
 
 def word_wrap_except_code_blocks(text: str) -> str:
     """Wraps text except for code blocks.
@@ -20,23 +20,23 @@ def word_wrap_except_code_blocks(text: str) -> str:
         The wrapped text.
     """
     # Split text into paragraphs
-    paragraphs = text.split('\n\n')
+    paragraphs = text.split("\n\n")
     wrapped_paragraphs = []
     # Check if currently in a code block.
     in_code_block = False
     # Loop through each paragraph and apply appropriate wrapping.
     for paragraph in paragraphs:
         # If this paragraph starts and ends with a code block, add it as is.
-        if paragraph.startswith('```') and paragraph.endswith('```'):
+        if paragraph.startswith("```") and paragraph.endswith("```"):
             wrapped_paragraphs.append(paragraph)
             continue
         # If this is the beginning of a code block add it as is.
-        if paragraph.startswith('```'):
+        if paragraph.startswith("```"):
             in_code_block = True
             wrapped_paragraphs.append(paragraph)
             continue
         # If this is the end of a code block stop skipping text.
-        if paragraph.endswith('```'):
+        if paragraph.endswith("```"):
             in_code_block = False
             wrapped_paragraphs.append(paragraph)
             continue
@@ -48,8 +48,9 @@ def word_wrap_except_code_blocks(text: str) -> str:
             wrapped_paragraph = textwrap.fill(paragraph)
             wrapped_paragraphs.append(wrapped_paragraph)
     # Join all paragraphs into a single string
-    wrapped_text = '\n\n'.join(wrapped_paragraphs)
+    wrapped_text = "\n\n".join(wrapped_paragraphs)
     return wrapped_text
+
 
 def read_lines(file_path: str, start_line: int, end_line: int) -> (str, int):
     """
@@ -65,7 +66,7 @@ def read_lines(file_path: str, start_line: int, end_line: int) -> (str, int):
 
     """
     # open the file for reading
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         # read all the lines from the file
         lines = f.readlines()
         # remove trailing newline characters
@@ -75,24 +76,31 @@ def read_lines(file_path: str, start_line: int, end_line: int) -> (str, int):
     # ensure end_line is within range
     end_line = min(len(lines), end_line)
     # return the requested lines as a list
-    return ('\n'.join(lines[start_line:end_line]) + '\n', start_line, end_line)
+    return ("\n".join(lines[start_line:end_line]) + "\n", start_line, end_line)
+
 
 def complete(args, user_prompt):
     try:
         completion = openai.ChatCompletion.create(
-            model=args['llm'],
-            request_timeout=args['timeout'],
-            messages=[{'role': 'user', 'content': user_prompt}])
+            model=args["llm"],
+            request_timeout=args["timeout"],
+            messages=[{"role": "user", "content": user_prompt}],
+        )
         return completion.choices[0].message.content
     except openai.error.AuthenticationError:
-        print('You need an OpenAI key to use this tool.')
-        print('You can get a key here: https://platform.openai.com/account/api-keys')
-        print('Set the environment variable OPENAI_API_KEY to your key value.')
-        print('If OPENAI_API_KEY is already correctly set, you may have exceeded your usage or rate limit.')
+        print("You need an OpenAI key to use this tool.")
+        print("You can get a key here: https://platform.openai.com/account/api-keys")
+        print("Set the environment variable OPENAI_API_KEY to your key value.")
+        print(
+            "If OPENAI_API_KEY is already correctly set, you may have exceeded your usage or rate limit."
+        )
     except openai.error.Timeout:
-        print('The OpenAI API timed out. You can try increasing the timeout with the --timeout option.')
+        print(
+            "The OpenAI API timed out. You can try increasing the timeout with the --timeout option."
+        )
 
     sys.exit(1)
+
 
 class explain_context(object):
     def __init__(self, diagnostic):
@@ -109,7 +117,7 @@ class explain_context(object):
         line = 0
         while line < len(diagnostic_lines):
             # This pattern works for some C++ compilers (GCC, Clang) and Rust.
-            match = re.search(r'^([^:->]+):([0-9]+):([0-9]+)', diagnostic_lines[line])
+            match = re.search(r"^([^:->]+):([0-9]+):([0-9]+)", diagnostic_lines[line])
 
             line += 1
 
@@ -124,31 +132,37 @@ class explain_context(object):
             file_name = match.group(1)
             line_number = int(match.group(2))
 
-            (abridged_code, line_start, line_end) = read_lines(file_name, line_number - 7, line_number + 2)
+            (abridged_code, line_start, line_end) = read_lines(
+                file_name, line_number - 7, line_number + 2
+            )
 
             # Avoid duplicates.
             if (file_name, line_start, line_end) not in self.code_locations:
-              self.code_locations[(file_name, line_start, line_end)] = abridged_code
-              max_code_locations -= 1
+                self.code_locations[(file_name, line_start, line_end)] = abridged_code
+                max_code_locations -= 1
 
         # If the diagnostic didn't come from a context that we know about and
         # handle in the above loop, we should make sure it's not too long.
         if not self.code_locations and line == len(diagnostic_lines) - 1:
             line = min(line, 50)
 
-        self.unabridged_diagnostic = ''.join(diagnostic_lines) + '\n'
-        self.abridged_diagnostic = '```\n' + ''.join(diagnostic_lines[:line]) + '```\n'
+        self.unabridged_diagnostic = "".join(diagnostic_lines) + "\n"
+        self.abridged_diagnostic = "```\n" + "".join(diagnostic_lines[:line]) + "```\n"
 
         def format_code_location(code_location):
             ((file_name, line_start, line_end), abridged_code) = code_location
-            s = 'File `{}` line {} to {}:\n'.format(file_name, line_start, line_end)
-            s += '\n'
-            s += '```\n'
+            s = "File `{}` line {} to {}:\n".format(file_name, line_start, line_end)
+            s += "\n"
+            s += "```\n"
             s += abridged_code
-            s += '```\n'
+            s += "```\n"
             return s
 
-        self.code = '\n'.join([format_code_location(cl) for cl in self.code_locations.items()]) + '\n'
+        self.code = (
+            "\n".join([format_code_location(cl) for cl in self.code_locations.items()])
+            + "\n"
+        )
+
 
 def base_prompt():
     with io.open(sys.stdin.fileno(), "rb", closefd=False) as stdin:
@@ -169,11 +183,17 @@ def base_prompt():
 
     return user_prompt
 
+
 def explain_prompt():
     return base_prompt() + "What's the problem?"
 
+
 def fix_prompt():
-    return base_prompt() + "Suggest code to fix the problem. Surround the code in backticks (```)."
+    return (
+        base_prompt()
+        + "Suggest code to fix the problem. Surround the code in backticks (```)."
+    )
+
 
 class extract_sources_context(object):
     def __init__(self, diagnostic):
@@ -182,8 +202,11 @@ class extract_sources_context(object):
 
         line = min(len(diagnostic_lines) - 1, 50)
 
-        self.unabridged_diagnostic = '\n'.join(diagnostic_lines) + '\n'
-        self.abridged_diagnostic = '```\n' + '\n'.join(diagnostic_lines[:line]) + '\n```\n'
+        self.unabridged_diagnostic = "\n".join(diagnostic_lines) + "\n"
+        self.abridged_diagnostic = (
+            "```\n" + "\n".join(diagnostic_lines[:line]) + "\n```\n"
+        )
+
 
 def extract_sources_prompt():
     with io.open(sys.stdin.fileno(), "rb", closefd=False) as stdin:
@@ -200,4 +223,3 @@ def extract_sources_prompt():
     user_prompt += ctx.abridged_diagnostic
 
     return user_prompt
-
