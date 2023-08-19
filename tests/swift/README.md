329329353552eabac1d32cce89e5340b90dede6b
Emery Berger <emery.berger@gmail.com>
2023-08-19 15:24:09 -0400

    Added Swift support (no need for new regexes).

---
+++ tests/swift/README.md
@@ -0,0 +1,1 @@
+Compile with `swiftc testme.swift`.
---
+++ tests/swift/testme.swift
@@ -0,0 +1,6 @@
+// Create a Dictionary with two elements
+var myDictionary = ["first_name": "Sergey", "last_name": "Kargopolov"]
+// Loop through dictionary keys and print values
+for (key,value) in myDictionar {
+    print("\(key) = \(value)")
+}
