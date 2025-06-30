da1969acc06e3bce4a5a5c496d3fc826c8e4fba5
Nicolas van Kempen <nvankempen@umass.edu>
2025-06-30 10:11:20 -0400

    Add Git support (#72)

--- src/cwhy/functions.py
+++ src/cwhy/functions.py
@@ -7,6 +7,7 @@ import llm_utils
 import openai
 
 from . import clangd_lsp_integration
+from . import git_integration
 
 
 class Functions:
@@ -23,6 +24,18 @@ class Functions:
 
         if clangd_lsp_integration.is_available():
             fns.append(self.find_definition)
+        else:
+            print(
+                "[WARNING] clangd is not available. The 'find_definition' function will not be made available."
+            )
+
+        if git_integration.is_available():
+            fns.append(self.git_status)
+            fns.append(self.git_diff)
+        else:
+            print(
+                "[WARNING] `git status` is not available. The associated git functions will not be made available."
+            )
 
         for f in fns:
             assert f.__doc__
@@ -55,6 +68,12 @@ class Functions:
                 return self.find_definition(
                     arguments["filename"], arguments["line_number"], arguments["symbol"]
                 )
+            elif function_call.name == "git_status":
+                return self.git_status()
+            elif function_call.name == "git_diff":
+                return self.git_diff(arguments["filename"])
+            else:
+                print(f"[ERROR] Unknown function: {function_call.name}")
         except Exception as e:
             print(e)
         return None
@@ -121,7 +140,7 @@ class Functions:
         result = "\n".join(entries)
         print(result)
         return result
-    
+
     def find_definition(self, filename: str, line_number: int, symbol: str) -> str:
         """
         {
@@ -148,6 +167,47 @@ class Functions:
             }
         }
         """
-        result = clangd_lsp_integration.definition_plus_heuristics(filename, line_number, symbol)
+        result = clangd_lsp_integration.definition_plus_heuristics(
+            filename, line_number, symbol
+        )
         print(result)
         return result
+
+    def git_status(self) -> str:
+        """
+        {
+            "description": "Returns the git status (--porcelain) of the current repository.",
+            "strict": true,
+            "parameters": {
+                "type": "object",
+                "properties": {},
+                "required": [],
+                "additionalProperties": false
+            }
+        }
+        """
+        status = git_integration.get_git_status_porcelain()
+        print(status)
+        return status
+
+    def git_diff(self, filename: str) -> str:
+        """
+        {
+            "description": "Returns the diff since the last commit of the given file.",
+            "strict": true,
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "filename": {
+                        "type": "string",
+                        "description": "The filename path to get the git diff for (relative to the current directory)."
+                    }
+                },
+                "required": ["filename"],
+                "additionalProperties": false
+            }
+        }
+        """
+        diff = git_integration.get_git_diff(filename)
+        print(diff)
+        return diff
